c25130e8603831ce5f9436eac303dafb8a6963a5
Dmitriy Yunashev <sys.dream2@gmail.com>
2019-08-15 18:35:31 +0300

    brain-even

---
+++ src/bin/brain-even.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { userName, getAnswerIsEven, brainEvenGame } from '..'
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".\n')
+brainEvenGame();
+
+
+  
+
+
+  
+
+
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import { userName } from '..'
-console.log('Welcome to the Brain Games!');
+import { greeting } from '..'
 
-console.log('Hello, ' + userName() + '!');
+console.log('Welcome to the Brain Games!\n');
+greeting();
 
